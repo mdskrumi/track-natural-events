b9131f2d4fba34319d7d4fa1cef24b27bd2c9ce6
Md Sakibul Alam <sakibul.alam@brainstation-23.com>
2022-02-22 13:28:01 +0600

    Add wildfire data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-
 import Map, {
   FullscreenControl,
   GeolocateControl,
@@ -13,6 +12,7 @@ import gsap from "gsap";
 
 // Images
 import OFFICE_IMAGE from "../assets/images/office.png";
+import FIRE_IMAGE from "../assets/images/fire.png";
 
 const MapWrapper = styled.div`
   width: 100vw;
@@ -22,14 +22,37 @@ const MapWrapper = styled.div`
 
 const LocationLatLongDiv = styled.div`
   background-color: rgba(255, 255, 255, 0);
+  color: black;
   position: absolute;
   top: 0;
   z-index: 1;
   margin: 5px 0px 0px 10px;
 `;
 
+const WildFireButtonDiv = styled.div`
+  background-color: rgba(255, 255, 255, 0);
+  color: black;
+  position: absolute;
+  bottom: 20px;
+  right: 10px;
+  z-index: 1;
+  margin: 5px 0px 0px 10px;
+  cursor: pointer;
+`;
+
+const WildFireImage = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
 interface HomePropsInterface {
   userLocation: GeolocationPosition | undefined;
+}
+
+interface WildFireData {
+  link: string;
+  date: string;
+  coordinates: number[];
 }
 
 const Home = (props: HomePropsInterface) => {
@@ -37,19 +60,32 @@ const Home = (props: HomePropsInterface) => {
   const [initialViewState, setInitialViewState] = useState({
     longitude: 90,
     latitude: 23,
-    zoom: 14,
+    zoom: 1,
   });
+  const [wildFireData, setWildFireData] = useState<WildFireData[]>();
+  const [showFireData, setShowFireData] = useState<boolean>(false);
   const mapWrapperDivRef = useRef<HTMLDivElement>(null);
 
+  const fetchFireData = async () => {
+    const response = await fetch(
+      "https://eonet.gsfc.nasa.gov/api/v2.1/events"
+    ).then((res) => res.json());
+    const wildFires: WildFireData[] = [];
+    response.events.forEach((evt: any) => {
+      if (evt.categories[0].id === 8) {
+        wildFires.push({
+          link: evt.sources[0].url,
+          date: evt.geometries[0].date,
+          coordinates: evt.geometries[0].coordinates,
+        });
+      }
+    });
+    setWildFireData(wildFires);
+  };
+
   useEffect(() => {
-    if (userLocation) {
-      setInitialViewState({
-        longitude: userLocation.coords.longitude,
-        latitude: userLocation.coords.latitude,
-        zoom: 14,
-      });
-    }
-  }, [userLocation]);
+    fetchFireData();
+  }, []);
 
   useEffect(() => {
     gsap
@@ -57,6 +93,16 @@ const Home = (props: HomePropsInterface) => {
       .to(mapWrapperDivRef.current, { duration: 2 })
       .to(mapWrapperDivRef.current, { opacity: 1, duration: 4 });
   }, []);
+
+  useEffect(() => {
+    if (userLocation) {
+      setInitialViewState({
+        longitude: userLocation.coords.longitude,
+        latitude: userLocation.coords.latitude,
+        zoom: 1,
+      });
+    }
+  }, [userLocation]);
 
   return (
     <MapWrapper ref={mapWrapperDivRef}>
@@ -67,10 +113,16 @@ const Home = (props: HomePropsInterface) => {
           3
         )} Zoom: ${initialViewState.zoom.toFixed(0)}`}
       </LocationLatLongDiv>
+      {wildFireData ? (
+        <WildFireButtonDiv onClick={() => setShowFireData(!showFireData)}>
+          Get fire data
+          <WildFireImage src={FIRE_IMAGE} alt="fire" width="20" height="15" />
+        </WildFireButtonDiv>
+      ) : null}
       <Map
         {...initialViewState}
         style={{ width: "100%", height: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapStyle="mapbox://styles/mapbox/streets-v10"
         mapboxAccessToken="pk.eyJ1IjoibWRza3J1bWkiLCJhIjoiY2t6cHJpODZrNWs4eTJ1cHE2d2gyamo3bCJ9._wPMQlwgpNT5DdxvacyRLQ"
         onMove={(evt) => setInitialViewState(evt.viewState)}
       >
@@ -84,6 +136,19 @@ const Home = (props: HomePropsInterface) => {
         />
         <ScaleControl />
         <NavigationControl visualizePitch />
+
+        {showFireData && wildFireData
+          ? wildFireData.map((wf) => (
+              <Marker
+                longitude={wf.coordinates[0]}
+                latitude={wf.coordinates[1]}
+                anchor="bottom"
+              >
+                <img src={FIRE_IMAGE} alt="wild fire" />
+              </Marker>
+            ))
+          : null}
+
         <Marker longitude={90.4001656} latitude={23.781855} anchor="bottom">
           <img src={OFFICE_IMAGE} alt="office" />
         </Marker>
